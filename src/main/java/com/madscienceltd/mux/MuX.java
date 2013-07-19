d198bb7fdf8360136919f00a0686e2f1fb0c5129
Maurice Brakels <m.brakels@hotmail.nl>
2013-07-20 00:37:43 +0200

    First Commit
    Signed-off-by: Maurice Brakels <m.brakels@hotmail.nl>

---
+++ src/main/java/com/madscienceltd/mux/MuX.java
@@ -0,0 +1,13 @@
+package com.madscienceltd.mux;
+
+/**
+ * Hello world!
+ *
+ */
+public class MuX 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Just begun, Let's play a bit Bigpoint S.a.r.l." );
+    }
+}
